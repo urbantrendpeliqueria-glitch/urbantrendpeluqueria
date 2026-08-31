6c0c8a2e1e9ed043966a06a1fa724bfadb099de0
urbantrendpeliqueria-glitch <urbantrendpeliqueria@gmail.com>
2026-08-31 18:34:46 -0300

    Proteger acceso administrativo

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,4 @@
 window.URBAN_TREND_SUPABASE = {
   url: "https://uhgdkuzoomaznzlyuxzo.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoZ2RrdXpvb21hem56bHl1eHpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDI1ODcsImV4cCI6MjEwMzE3ODU4N30.tB0eBzr90C7-yHLZ8V8b_iqEst6b_CImfN-As24FKp8",
-  adminEmail: "urbantrendpeluqueria@gmail.com"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoZ2RrdXpvb21hem56bHl1eHpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDI1ODcsImV4cCI6MjEwMzE3ODU4N30.tB0eBzr90C7-yHLZ8V8b_iqEst6b_CImfN-As24FKp8"
 };
